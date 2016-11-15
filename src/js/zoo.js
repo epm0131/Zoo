dfc4191a7be59924d70830c992bbd8e47e5e794f
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-15 17:19:33 -0500

    I added the layegg functionality and also breath fire!

--- src/js/zoo.js
+++ src/js/zoo.js
@@ -2,19 +2,19 @@
   'use strict';
 
   function Creature(name, dob) {
-    console.log('inside creature', this);
+    console.log(this);
     this.name = name || 'nameless';
     this.dob = dob || 'not alive';
   };
 
-  var draco = new Creature('draco', 'dob');
+  var draco = new Creature('draco', 1603);
   // var daedalus = new Creature('daedalus', 'dob');
 
   function Dragon(name, dob) {
     Creature.apply( this, [name] );
+    this.dob = dob;
 
   };
-
   // function Minotaur(name, dob) {
   //   this.name = name || 'nameless';
   // };
@@ -22,10 +22,23 @@
   Dragon.prototype = Object.create(Creature.prototype);
   Dragon.prototype.constructor = Dragon;
 
-  var dragon = new Dragon('draco', 'dob');
+  Dragon.prototype.layEgg = function layEgg() {
+    var babyDragon = new Dragon('drago', 2016);
+    return babyDragon;
+  };
+
+  Dragon.prototype.breathFire = function breathFire() {
+    return 'Fire!!!!!!!!!!!!';
+  }
+
+  var dragon = new Dragon('draco', 1603);
   // var daedalus = new Minotaur('daedalus', 'dob');
 
   console.log('draco:', draco);
+  console.log(dragon.layEgg());
+  console.log(dragon.breathFire());
+
+
   // console.log('daedalus:', daedalus);
 
 
